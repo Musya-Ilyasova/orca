c2e5a03167d903e07010c994d9182a5f958fcf0a
Musya <musya.ilyasova@getorca.app>
2021-02-17 12:47:01 +0300

    add

--- src/js/modules/rewarded-slider.js
+++ src/js/modules/rewarded-slider.js
@@ -1,8 +1,34 @@
-const swiper = new Swiper('.rewarded-slider-container ', {
-  speed: 400,
-  spaceBetween: 10,
-  slidesPerView: 'auto',
-  pagination: {
-    el: '.swiper-pagination',
-  },
-});
+// const swiper = new Swiper('.rewarded-slider-container', {
+//   speed: 400,
+//   spaceBetween: 10,
+//   slidesPerView: 'auto',
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+// });
+
+(function () {
+  "use strict";
+  const breakpoint = window.matchMedia("(min-width:768px)");
+  let rewardedSlider;
+  const breakpointChecker = function () {
+    if (breakpoint.matches === true) {
+      if (rewardedSlider !== undefined) rewardedSlider.destroy(true, true);
+      return;
+    } else if (breakpoint.matches === false) {
+      return enableTopSwiper();
+    }
+  };
+  const enableTopSwiper = function () {
+    rewardedSlider = new Swiper(".rewarded-slider-container", {
+      speed: 400,
+      spaceBetween: 10,
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+  };
+  breakpoint.addListener(breakpointChecker);
+  breakpointChecker();
+})();

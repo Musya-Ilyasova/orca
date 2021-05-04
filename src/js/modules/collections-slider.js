6c6fdd2f370c84316b05a822f75e0f598f0ec6ae
Musya <musya.ilyasova@getorca.app>
2021-05-04 20:47:20 +0300

    add

--- src/js/modules/collections-slider.js
+++ src/js/modules/collections-slider.js
@@ -6,8 +6,25 @@ const swiper = new Swiper('.collections-slider-container', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.collections-slider-next',
+    prevEl: '.collections-slider-prev',
   },
+  on: {
+    init: addRotate(),
+  }
 
 });
+
+function addRotate() {
+  let collectionImg = document.querySelectorAll('.collections-slider-item-img');
+  // for(let i=1; i<=collectionImg.length; i++) {
+  //   console.log(i);
+  // }
+  for(let i=1; i<=collectionImg.length; i++) {
+    if(i%2 ===0) {
+      collectionImg[i].style.transform = 'rotate(2deg)';
+    } else {
+      collectionImg[i].style.transform = 'rotate(-2deg)';
+    }
+  }
+}

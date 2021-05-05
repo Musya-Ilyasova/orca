746d11ca2d6880732cc670d18161d42c922cf3e6
Musya <musya.ilyasova@getorca.app>
2021-05-05 17:24:26 +0300

    add css and animation slider

--- src/js/modules/collections-slider.js
+++ src/js/modules/collections-slider.js
@@ -1,30 +1,80 @@
-const swiper = new Swiper('.collections-slider-container', {
-  // Optional parameters
-  lidesPerView: 1,
-  spaceBetween: 22,
-  // If we need pagination
+function buildCollectionsSwiper() {
+  (function () {
+    "use strict";
+    var breakpoint = window.matchMedia("(min-width:768px)");
+    var collectionsSwiper;
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.collections-slider-next',
-    prevEl: '.collections-slider-prev',
-  },
-  on: {
-    init: addRotate(),
-  }
+    var breakpointChecker = function breakpointChecker() {
+      if (breakpoint.matches === true) {
+        if (breakpointChecker !== undefined) {
+          window.onresize = function() {
+            collectionsSwiper.destroy(true, true);
+          }
+          return;
+        }
+      } else if (breakpoint.matches === false) {
+        return enableCollectionsSwiper();
+      }
+    };
 
-});
+    var enableCollectionsSwiper = function enableCollectionsSwiper() {
+      collectionsSwiper = new Swiper(".collections-slider-container", {
+        lidesPerView: 1,
+        spaceBetween: 22,
+        effect: 'fade',
+        speed: 1000,
+        allowTouchMove: false,
+        fadeEffect: {
+          crossFade: true
+        },
+        navigation: {
+          nextEl: '.collections-slider-next',
+          prevEl: '.collections-slider-prev',
+        },
+        // on: {
+        //   slideChangeTransitionStart: function() {
+        //     let slide = document.querySelectorAll('.collections-slider-item');
+        //     slide.forEach(i=> {
+        //       i.style.opacity="0";
+        //     })
+        //   },
+        //   slideChangeTransitionEnd: function() {
+        //     let slide = document.querySelectorAll('.collections-slider-item');
+        //     slide.forEach(i=> {
+        //       i.style.opacity="1";
+        //     })
+        //   }
+        // }
+      });
+    };
 
+    breakpoint.addListener(breakpointChecker);
+    breakpointChecker();
+  })();
+}
 function addRotate() {
   let collectionImg = document.querySelectorAll('.collections-slider-item-img');
-  // for(let i=1; i<=collectionImg.length; i++) {
-  //   console.log(i);
-  // }
-  for(let i=1; i<=collectionImg.length; i++) {
-    if(i%2 ===0) {
-      collectionImg[i].style.transform = 'rotate(2deg)';
-    } else {
+  for(let i=0; i<=collectionImg.length-1; i++) {
+    if(i%2 === 0) {
       collectionImg[i].style.transform = 'rotate(-2deg)';
+    } else {
+      collectionImg[i].style.transform = 'rotate(2deg)';
     }
-  }
-}
+  };
+};
+addRotate();
+buildCollectionsSwiper();
+
+// const collectionsSwiper = new Swiper('.collections-slider-container', {
+//   lidesPerView: 1,
+//   spaceBetween: 22,
+//   navigation: {
+//     nextEl: '.collections-slider-next',
+//     prevEl: '.collections-slider-prev',
+//   },
+//   on: {
+//     init: addRotate(),
+//   }
+// });
+
+

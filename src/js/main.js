3ad3fc44f0421262aa37a594323fce482dafc7f7
Musya <abka-musli@yandex.ru>
2021-06-04 02:03:30 +0300

    add seo

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,18 @@ if(document.body.classList.contains("page-allocation")) {
   @@include('modules/pageShareXhr.js')
   @@include('modules/aside.js')
   @@include('modules/copyAllocation.js')
-  @@include('modules/chart-tooltip.js')
-  
+  @@include('modules/chart-tooltip.js') 
+};
+
+
+if(document.body.classList.contains("page-seo")) {
+  function faqBtnToggle() {
+    document.querySelectorAll('.faq-list-item-top').forEach((item) => {
+      item.addEventListener('click', function(){
+        this.querySelector('.faq-list-item__btn').classList.toggle('open');
+        this.parentNode.querySelector('.faq-list-item__description').classList.toggle('show');
+      })
+    });
+  };
+  faqBtnToggle();
 }

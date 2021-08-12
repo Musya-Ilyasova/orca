8fb02a55c71bdc87841b505d091ff1aed1b803d4
Musya <abka-musli@yandex.ru>
2021-08-13 01:21:04 +0300

    add

--- src/js/main.js
+++ src/js/main.js
@@ -46,3 +46,15 @@ if(document.body.classList.contains("page-gia")) {
   addGiaSlider();
 }
 
+
+function faqBtnToggle() {
+  document.querySelectorAll('.faq-list-item-top').forEach((item) => {
+    item.addEventListener('click', function(){
+      this.querySelector('.faq-list-item__btn').classList.toggle('open');
+      this.parentNode.querySelector('.faq-list-item__description').classList.toggle('show');
+    })
+  });
+};
+if(document.body.classList.contains("page-main")) {
+  faqBtnToggle();
+}

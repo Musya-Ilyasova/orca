9297d5c111bbd91ba9b56aad81ce7f7517692aad
Musya <abka-musli@yandex.ru>
2021-11-04 03:06:43 +0300

    add team

--- src/js/modules/team.js
+++ src/js/modules/team.js
@@ -12,22 +12,31 @@ function teamRotate() {
   function rotateItem(li) {
     card = li.closest('.team-list-item:not(.notBack)');
     card.classList.add('rotate');
-    card.addEventListener('onmouseleave', cardRemoveRotate(card));
-      // card.onmouseleave = function (e) {
-      //   setTimeout(() => {
-      //     this.classList.remove('rotate');
-
-      //   }, 500)
-      // }
-
-    function cardRemoveRotate(item) {
-      if(item.classList.contains('rotate')) {
-        setTimeout(() => {
-          this.classList.remove('rotate');
-        }, 500)
-      }
+    card.onmouseleave = function() {
+      setTimeout(() => card.classList.remove('rotate'), 200)
     }
+  }
+}
+function teamMore() {
+ let btn = document.querySelectorAll('.team__more');
+ let teamList = document.querySelectorAll('.team-list');
+ btn.forEach((i) => {
+   i.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    if(i.previousElementSibling.closest('.team-list') && !i.classList.contains('less')) {
+      i.previousElementSibling.style.maxHeight = i.previousElementSibling.scrollHeight + 20 + 'px';
+      i.classList.add('less');
+      i.querySelector('span').textContent = "Show less";
+    } else if(i.classList.contains('less')) {
+      i.previousElementSibling.style.maxHeight = '';
+      i.classList.remove('less');
+      i.querySelector('span').textContent = "Show more";
+    }
+   })
+ })
 }
 
 teamRotate();
+teamMore();
 

4fd1f4273587efe1c868609645744170f241fc23
Musya <abka-musli@yandex.ru>
2021-11-30 13:10:30 +0300

    add files

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,32 @@
-if(document.body.classList.contains("page-main")) {
-  faqBtnToggle();
-  mainHeaderBtnToggle();
-  if (window.innerWidth < 768) {
-    scrollPage ();
-  }
+function sendEmailForm() { 
+  let form = document.querySelector('form');
+  let node = form.querySelector('button');
+  const XHR = new XMLHttpRequest();
+  const FD = new FormData(form);
+  XHR.addEventListener("loadend", function (event) {
+    const response = XHR.response;
+    if (response) {
+      if (XHR.status == 200) {
+        document.querySelector('.field-wrapper').style.display="none";
+        document.querySelector('.success').style.display="flex"
+      } else {
+        form.querySelector('.error-request').style.display = 'block';
+      } 
+    }else {
+      form.querySelector('.error-request').style.display = 'block';
+    } 
+  });
+  XHR.open("POST", form.action);
+  XHR.send(FD);
+  node.disabled = false;
 }
 
 
-@@include('modules/team.js');
+function comicconForm() {
+  let btn = document.querySelector('.comiccon-subscribe-btn button');
+  btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    sendEmailForm()
+  })
+}
+comicconForm();

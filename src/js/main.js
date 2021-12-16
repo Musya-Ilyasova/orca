25bbe19369c748a80ab9673101be3b9f136ea3d6
Musya <abka-musli@yandex.ru>
2021-12-16 03:20:51 +0300

    add

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,27 @@
 function yellowsEyes() {
   let section = document.querySelector('.us-add');
-  let img = document.querySelector('.us-add-img-1 span i'),
+  let wrapper = document.querySelector('.us-add-img-1 span:nth-of-type(2)').getBoundingClientRect(),
+  img = document.querySelector('.us-add-img-1 span:nth-of-type(2) i'),
   imgX = img.offsetLeft+(img.offsetWidth),
   imgY = img.offsetTop+(img.offsetHeight);
-  console.log(img.offsetLeft)
+
+  console.log(wrapper);
   section.addEventListener('mousemove', function(e) {
     let x = (e.clientX - imgX)/100;
     let y = (e.clientY - imgY)/100;
-    img.style.transform = `translate3d(${x * 3}px, ${y * 3}px, 0 )`;
-    console.log(x);
-    console.log(y);
+    img.style.transform = `translate3d(${x * 5}px, ${y * 5}px, 0 )`;
+    // console.log(x);
+    // console.log(y);
 
     // img.forEach(i => {
     //   i.style.transform = `translate3d(${x * 2}px, ${y * 2}px, 0 )`;
     // });
     // document.querySelector('.intro-referral_image_logotypes').style.transform = `translate3d(${x * 3}px, ${y * 3}px, 0 )`;
     // document.querySelector('.intro-referral_image_gifts').style.transform = `translate3d(${x * 4}px, ${y * 4}px, 0 )`;
-  })
+  });
+  section.addEventListener('mouseleave', function() {
+    img.style.transform = `translate3d(0, 0, 0 )`;
+  });
 }
 
 yellowsEyes();
